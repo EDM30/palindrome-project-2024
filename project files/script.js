94819ba50dfddda64a2621a7dfd6b8111414fce4
Emerson <edavid.manaog@gmail.com>
2024-07-26 18:02:01 +1000

    completed project

--- project files/script.js
+++ project files/script.js
@@ -1,12 +1,82 @@
+function remove(str) {
+    let res = '';
+    for (let i = 0; i < str.length; i++) {
+        let character = str.charAt(i);
+        if (!checkPunctuation(character) && character !== ' ') {
+            res += character;
+        }
+    }
+    return res;
+}
+
+function checkPunctuation(char) {
+    const punctuation = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
+    return punctuation.includes(char);
+}
+
 function checkPalindrome() {
-    const word = document.getElementById('wordInput').value.toLowerCase();
-    const reversedWord = word.split('').reverse().join('');
+    const input = document.getElementById('inputText').value;
+    const cleanInput = remove(input.toLowerCase());
+    console.log("clean input: "+ cleanInput )
+    
+    let reversedInput = '';
+    for (let i = cleanInput.length - 1; i >= 0; i--) {
+        reversedInput += cleanInput[i];
+    }
+
     const result = document.getElementById('result');
 
-    if (word === reversedWord) {
-        result.textContent = `${word} is a palindrome!`;
+    if (cleanInput === reversedInput) {
+        result.textContent = `"${input}" is a palindrome!`;
     } else {
-        result.textContent = `${word} is not a palindrome.`;
+        result.textContent = `"${input}" is not a palindrome.`;
     }
 }
 
+
+
+
+
+
+
+// function checkPalindrome() {
+//     const word = document.getElementById('wordInput').value.toLowerCase();
+//     const reversedWord = word.split('').reverse().join('');
+//     const noSpaces = word.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+//     const result = document.getElementById('result');
+
+//     if (word === noSpaces) {
+//         result.textContent = `${word} is a palindrome!`;
+//     } else {
+//         result.textContent = `${word} is not a palindrome.`;
+//     }
+// }
+
+
+// https://www.geeksforgeeks.org/how-to-remove-punctuation-from-text-using-javascript/
+// function remove(str) {
+//     let res = '';
+//     for (let i = 0; i < str.length; i++) {
+//         let character = str.charAt(i);
+//         if (!checkPunctuation(character)) {
+//             res += character;
+//         }
+//      }
+    // console.log("remove: "+ res)
+//     return res;
+// }
+
+// function checkPunctuation(char) {
+//     const punctuation = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
+//     console.log(punctuation.includes(char));
+//     return punctuation.includes(char);
+// }
+
+// let str = "Welcome, to the geeksforgeeks!!!";
+
+// console.log(remove(str));
+
+
+
+
+// a man a plan a canal panama
